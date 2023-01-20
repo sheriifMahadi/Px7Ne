84487d91e5657f5c5a03dbe255364b75a406ceba
Abdulmajid Mahadi <70480860+sheriifMahadi@users.noreply.github.com>
2023-01-20 18:08:24 +0100

    Update db.js

--- src/data/db.js
+++ src/data/db.js
@@ -1,6 +1,6 @@
 import auction from '../data/auction.gif'
 import event from '../data/event.gif'
-
+import z-note from '../data/z-notes.gif'
 const projects = {
   "data": [
     {
@@ -23,22 +23,13 @@ const projects = {
      },
     //  {
     //   id: 3,
-    //   title: "Auction app",
-    //   description: "Auction app built using Django and JavaScript",
-    //   image: auction,
-    //   github: "https://github.com/sheriifMahadi/django-auction-app",
-    //   url: "https://mahadi.pythonanywhere.com/",
-    //   alt: "auction-app-img"
+    //   title: "Z-notes",
+    //   description: "A notes app built using Nodejs, React and RTK.",
+    //   image: z-note,
+    //   github: "https://github.com/sheriifMahadi/notes_project_frontend",
+    //   url: "https://z-note.netlify.app/",
+    //   alt: "z-notes-img"
     //  },
-    //  {
-    //    id: 4,
-    //    title: "event notifier app",
-    //    description: "A birthday tracker app with user functionalities and crud operations",
-    //    image: event,
-    //    github: "https://github.com/sheriifMahadi/event_notifier",
-    //    url: "https://mahadisheriff.pythonanywhere.com/",
-    //    alt: "event-app-img"
-    //   }
   ]
 }
 
